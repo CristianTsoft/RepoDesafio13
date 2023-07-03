6d0c3014f3aeca7680427c06310b4437c24551e9
anatsoft <ana.sincich@tsoftglobal.com>
2023-07-03 18:59:39 -0300

    nuevos cambios

--- src/test/java/aut/testcreation/testcases/TestVuelos.java
+++ src/test/java/aut/testcreation/testcases/TestVuelos.java
@@ -1,6 +1,7 @@
 package aut.testcreation.testcases;
 
 import aut.testcreation.pages.HomePage;
+import aut.testcreation.pages.VuelosPage;
 import framework.engine.selenium.DriverFactory;
 import framework.engine.selenium.SeleniumTestBase;
 import org.junit.jupiter.api.Test;
@@ -10,55 +11,17 @@ import org.junit.jupiter.api.Test;
 public class TestVuelos extends SeleniumTestBase {
 
     private HomePage homepage;
+    private VuelosPage vuelospage;
     @Test
     public void RV001 () {
         homepage = new HomePage(DriverFactory.getDriver());
+        vuelospage = new VuelosPage(homepage.getDriver());
         homepage.navigateTo("https://www.rumbo.es");
         homepage.noCookies();
         homepage.irAVuelos();
-        btnBusqueda.click();
-        this.homepage.irAVuelos();
+        vuelospage.Destino();
 
-    }
-
-    @Test
-    public void RV002 () {
-        homepage = new HomePage(DriverFactory.getDriver());
-        homepage.navigateTo("https://www.rumbo.es");
-        homepage.noCookies();
-        homepage.irAVuelos();
-    }
-
-    @Test
-    public void RV003 () {
-        homepage = new HomePage(DriverFactory.getDriver());
-        homepage.navigateTo("https://www.rumbo.es");
-        homepage.noCookies();
-        homepage.irAVuelos();
-    }
-
-    @Test
-    public void RV004 () {
-        homepage = new HomePage(DriverFactory.getDriver());
-        homepage.navigateTo("https://www.rumbo.es");
-        homepage.noCookies();
-        homepage.irAVuelos();
-    }
-
-    @Test
-    public void RV005 () {
-        homepage = new HomePage(DriverFactory.getDriver());
-        homepage.navigateTo("https://www.rumbo.es");
-        homepage.noCookies();
-        homepage.irAVuelos();
-    }
-
-    @Test
-    public void RV006 () {
-        homepage = new HomePage(DriverFactory.getDriver());
-        homepage.navigateTo("https://www.rumbo.es");
-        homepage.noCookies();
-        homepage.irAVuelos();
+        vuelospage.Busqueda();
     }
 
 }
